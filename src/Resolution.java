5b06ae6e123c249bc8982e5b2852faaa82481cf3
mp8671 <96391626+MaksimPikovskiy@users.noreply.github.com>
2022-03-11 16:43:47 -0500

    Removed "debug" statements (aka println's)
    Further simplified some logic

--- src/Resolution.java
+++ src/Resolution.java
@@ -14,7 +14,6 @@ public class Resolution {
         discardTautologies(clauses);
 
         while(true) {
-            //System.out.println("\nAll Clauses: " + clauses);
             List<Clause> clausesList = new LinkedList<>(clauses);
 
             for(int i = 0; i < clausesList.size() - 1; i++) {
@@ -23,15 +22,9 @@ public class Resolution {
                     Clause clause_j = clausesList.get(j);
                     Set<Clause> resolvents = new LinkedHashSet<>();
 
-                    System.out.println("\n\t" + clause_i + " AND " + clause_j);
-                    System.out.println("\n\tNew Clauses: " + newClauses);
-
-                    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
                     resolvents.addAll(resolve(clause_i, clause_j));
                     resolvents.addAll(resolve(clause_j, clause_i));
-                    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 
-                    //System.out.println("\t Resolvents: " + resolvents);
                     for(Clause resolvent : resolvents) {
                         if(resolvent.equals(Clause.EMPTY)) {
                             return "no";
@@ -41,22 +34,7 @@ public class Resolution {
                     newClauses.addAll(resolvents);
                 }
             }
-            System.out.println("\tClauses: " + clauses);
-            System.out.println("\tNew Clauses: " + newClauses);
-            System.out.println("\tClauses has New Clauses: " + clauses.containsAll(newClauses));
-            List<Clause> list1 = new ArrayList<>(clauses);
-            List<Clause> list2 = new ArrayList<>(newClauses);
-            boolean flag = false;
-            for(int i = 0; i < list1.size(); i++) {
-                for(int j = 0; j < list2.size(); j++) {
-                    System.out.println("\t\t" + list1.get(i) + " == " + list2.get(j));
-                    System.out.println("\t\t" + list1.get(i).equals(list2.get(j)));
-                    if(list1.get(i).equals(list2.get(j))) {
-                        flag = true;
-                    }
-                }
-            }
-            System.out.println("\tClauses has New Clauses: " + flag);
+
             if (clauses.containsAll(newClauses)) {
                 return "yes";
             }
@@ -68,59 +46,30 @@ public class Resolution {
     private Set<Clause> resolve(Clause clause_i, Clause clause_j) {
         Set<Clause> resolvents = new LinkedHashSet<>();
 
-        Set<Predicate> set1 = new LinkedHashSet<>(clause_i.getPositivePredicates());
-        Set<Predicate> set2 = new LinkedHashSet<>(clause_j.getNegativePredicates());
-        System.out.println("positive preds: " + set1);
-        System.out.println("negativae preds: " + set2);
-        Set<Predicate> complementarySet = new LinkedHashSet<>(set1);
-        if(set1 != set2) {
-            complementarySet.retainAll(set2);
+        Set<Predicate> complementarySet = new LinkedHashSet<>(clause_i.getPositivePredicates());
+        Set<Predicate> tempSet = new LinkedHashSet<>(clause_j.getNegativePredicates());
+        if(complementarySet != tempSet) {
+            complementarySet.retainAll(tempSet);
         }
 
-        System.out.println("Complementary Set: " + complementarySet);
-
         for(Predicate complement : complementarySet) {
-            System.out.println("complement: " + complement);
             List<Predicate> resolventLiterals = new LinkedList<>();
 
-
             for(Predicate ciPred : clause_i.getPredicates()) {
-                System.out.println("ciPred: " + ciPred);
                 if(ciPred.isNegated() || !ciPred.equals(complement)) {
                     resolventLiterals.add(ciPred);
                 }
             }
 
             for(Predicate cjPred : clause_j.getPredicates()) {
-                System.out.println("cjPred: " + cjPred);
                 if(!cjPred.isNegated() || !cjPred.equals(complement)) {
                     resolventLiterals.add(cjPred);
                 }
             }
 
             Clause resolvent = new Clause(resolventLiterals, kb);
-            System.out.println("Add Resolvent: " + resolvent);
-            System.out.println("Add Resolvents: " + resolvents);
             if(!resolvent.isTautology()) {
-                resolvents.add(resolvent); //calls clause.equals
-            }
-        }
-
-//        try {
-//            Thread.sleep(2500);
-//        } catch (InterruptedException e) {
-//
-//        }
-
-        if(resolvents.isEmpty()) {
-            System.out.println("Resolvents: none");
-        }
-        else {
-            System.out.println("Resolvents: " + resolvents);
-            List<Clause> temp = new ArrayList<>(resolvents);
-            if(temp.size() == 2) {
-                System.out.println(temp.get(0) + "is equal to " + temp.get(1));
-                System.out.println(temp.get(0).equals(temp.get(1)));
+                resolvents.add(resolvent);
             }
         }
 
